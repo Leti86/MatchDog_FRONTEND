47a420354f7414f0a4ef1a4c9962e0d9610588f9
Leti86 <leticia.anchuelo@gmail.com>
2020-11-27 23:56:23 +0100

    LETI: validaciones formulario registro protectora

--- src/app/login-protectora/login-protectora.component.ts
+++ src/app/login-protectora/login-protectora.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-protectora',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginProtectoraComponent implements OnInit {
 
-  constructor() { }
+  formRegistroProtectoras: FormGroup;
+
+  constructor() {
+    this.formRegistroProtectoras = new FormGroup({
+      nombre: new FormControl(
+        '', [Validators.required]
+      ),
+      email: new FormControl(
+        '', [
+        Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+      ]),
+      telefono: new FormControl(
+        '', [
+        Validators.required,
+        Validators.minLength(9)
+      ]
+      ),
+      direccion: new FormControl(
+        '', [Validators.required]
+      ),
+      localidad: new FormControl(
+        '', [Validators.required]
+      ),
+      provincia: new FormControl(
+        '', [Validators.required]
+      ),
+      latitud: new FormControl(
+        '', [Validators.required]
+      ),
+      longitud: new FormControl(
+        '', [Validators.required]
+      ),
+      necesidad_voluntarios: new FormControl(
+        '', [Validators.required]
+      ),
+      imagen: new FormControl(
+        '', [Validators.required]
+      ),
+      comentarios: new FormControl(
+        '', [
+        Validators.required,
+        Validators.minLength(20)
+      ]
+      )
+
+    })
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    console.log(this.formRegistroProtectoras.value);
+
   }
 
 }
