c6f7df8c8efba1795ba2041e99230ab8fbf69c34
DESKTOP-SLVH9SS\irene <irenedrf@gmail.com>
2020-12-01 16:13:56 +0100

    irene_nuestrosAnimales_maquetado

--- src/app/animales/animales.component.ts
+++ src/app/animales/animales.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-animales',
   templateUrl: './animales.component.html',
   styleUrls: ['./animales.component.css']
 })
 export class AnimalesComponent implements OnInit {
-
-  constructor() { }
+  filtroEdad: string;
+  filtroTamano: string;
+  constructor() {
+    this.filtroEdad = '';
+    this.filtroTamano = '';
+  }
 
   ngOnInit(): void {
   }
+
+  onClickEdad(pEdad: string) {
+    this.filtroEdad = pEdad;
+  }
+
+  onClickTamano(pTamano: string) {
+    this.filtroTamano = pTamano;
+  }
+
+
 
 }
